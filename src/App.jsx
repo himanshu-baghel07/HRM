e19cb877de046adb4fed4c0f95ca82ac20d19e98
Himanshu Baghel <himbaghel19@gmail.com>
2024-01-07 23:47:09 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect, useState } from "react";
+
+const App = () => {
+  useEffect(() => {
+    // meth();
+  });
+
+  return <div>Hrm</div>;
+};
+
+export default App;
