4e8f16c0545af115fd4f2d77201c49c9297ccd0f
Himanshu Baghel <122162767+himanshu-baghel07@users.noreply.github.com>
2024-01-17 20:50:03 +0530

    Revert "Himanshu"

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,12 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Dashboard from "./Pages/Dashboard/Dashboard";
-import Profile from "./Pages/Profile/Profile";
-import Home from "./Pages/Home/Home";
+import { useEffect, useState } from "react";
 
 const App = () => {
-  return (
-    <div>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Dashboard />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/home" element={<Home />} />
-        </Routes>
-      </Router>
-    </div>
-  );
+  useEffect(() => {
+    // meth();
+  });
+
+  return <div>Hkkk</div>;
 };
 
 export default App;
